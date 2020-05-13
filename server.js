74ed1d87cc3a8d05d64af7eed078630978406c89
rcarrata <rcarrata@redhat.com>
2020-05-13 21:07:03 +0200

    added socket io for add the server to push the messages to the front

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -12,16 +14,25 @@ var messages = [
     {name: 'Tim', message: 'Hello'},
 ]
 
+// Get messages
 app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
+// Post messages 
 app.post('/messages', (req, res) => {
-    console.log(req.body)
+    //console.log(req.body)
+    io.emit('message', req.body)
     messages.push(req.body)
     res.sendStatus(200)
 })
 
-var server = app.listen(3000, () => {
+// Socket io connections
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+// Initialization of the server
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 }) 
